ada366c9bd1c38f785bccfb2358f02f1ef179053
Matthias Kreier <matthias@kreier.org>
2022-11-11 20:42:50 +0700

    first shape drawn in preview

---
+++ badge/BadgeBackground.swift
@@ -0,0 +1,40 @@
+//
+//  BadgeBackground.swift
+//  badge
+//
+//  Created by Matthias Kreier on 11/11/2022.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * 0.20
+                )
+            )
+            
+            HexagonParameters.segments.forEach { segment in
+                path.addLine(
+                    to: CGPoint(
+                        x: width  * segment.line.x,
+                        y: height * segment.line.y
+                    )
+                )
+            }
+
+        }
+        .fill(.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
