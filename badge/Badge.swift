c07682a86a20e6f13810db5246f327f563a18225
Matthias Kreier <matthias@kreier.org>
2022-11-11 21:25:55 +0700

    8 rotations done!

--- badge/Badge.swift
+++ badge/Badge.swift
@@ -9,17 +9,25 @@ import SwiftUI
 
 struct Badge: View {
     var badgeSymbols: some View {
-        RotatedBadgeSymbol(angle: Angle(degrees: 0))
-            .opacity(0.5)
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
+        }
+        .opacity(0.5)
     }
     
     var body: some View {
         ZStack {
             BadgeBackground()
             
-            badgeSymbols
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
         }
-//        BadgeBackground()
+        .scaledToFit()
     }
 }
 
