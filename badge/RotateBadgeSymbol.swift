16e8805e97cdf6b783cd7c3ee3cf871acdfd11a9
Matthias Kreier <matthias@kreier.org>
2022-11-11 21:14:10 +0700

    rotated badge symbol

---
+++ badge/RotateBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotateBadgeSymbol.swift
+//  badge
+//
+//  Created by Matthias Kreier on 11/11/2022.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
