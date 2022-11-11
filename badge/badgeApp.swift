a79fed1f38bc5e774ac57d80bcb91d7e14189e89
Matthias Kreier <matthias@kreier.org>
2022-11-11 20:13:34 +0700

    Initial Commit

---
+++ badge/badgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  badgeApp.swift
+//  badge
+//
+//  Created by Matthias Kreier on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct badgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
